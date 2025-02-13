2223e97b7e0968a9cd5b09375d533cee86a5d210
Tu Nombre <marcoenriquez561290@gmail.com>
2025-02-13 10:50:57 -0500

    first commit

---
+++ src/main/kotlin/com/example/artribackend/config/CorsConfig.kt
@@ -0,0 +1,25 @@
+package com.example.artribackend.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+
+@Configuration
+class CorsConfig {
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource? {
+        val corsConfiguration = CorsConfiguration()
+
+        corsConfiguration.allowedOrigins = listOf("http://localhost:5173", "http://artri.sudamericano.edu.ec:5173", "http://34.125.86.215:5173", "https://artri.sudamericano.edu.ec")
+        corsConfiguration.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "PATCH")
+        corsConfiguration.allowedHeaders = listOf("*")
+        corsConfiguration.allowCredentials = true
+
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/", corsConfiguration)
+
+        return source
+    }
+}
